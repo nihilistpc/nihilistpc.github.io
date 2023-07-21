36f19edd252bb689f6417d2bca81241c5865e69e
nihilistpc <75069109+nihilistpc@users.noreply.github.com>
2023-07-21 18:44:26 +0400

    01

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,5 @@
-// Custom theme code
-
-if (document.getElementsByClassName('clean-gallery').length > 0) {
-   baguetteBox.run('.clean-gallery', { animation: 'slideIn' });
-}
-
-if (document.getElementsByClassName('clean-product').length > 0) {
-    window.onload = function() {
-        vanillaZoom.init('#product-preview');
-    };
-}
+document.querySelectorAll('.datepicker').forEach(function(field) {
+	var picker = new Pikaday({
+		field: field
+	});
+});
